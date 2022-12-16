86e2652e31063587102112ea00748763317a58a4
Luofan Chen <clfbbn@gmail.com>
2022-12-16 22:56:41 +0530

    gta4lwifi: Ship android.system.keystore2
    Fixes: Could not find 'aidl/android.system.keystore2.IKeystoreService/default'
           for ctl.interface_start from pid: 617 (/system/bin/servicemanager)

--- device.mk
+++ device.mk
@@ -14,6 +14,10 @@ PRODUCT_PACKAGES += \
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Keystore
+PRODUCT_PACKAGES += \
+    android.system.keystore2
+
 # Qcom decryption
 PRODUCT_PACKAGES += \
     qcom_decrypt \
