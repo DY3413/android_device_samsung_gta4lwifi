68e08088c4ff2a66f5f9e502096b7e89919b2af8
DY3413 <milosix3413@gmail.com>
2023-10-08 12:48:45 +0900

    BoardConfig: Bring up for bengal
    Signed-off-by: DY3413 <milosix3413@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,3 +4,33 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+DEVICE_PATH := device/samsung/gta4lwifi
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a73
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a73
+
+ENABLE_CPUSETS := true
+ENABLE_SCHEDBOOST := true
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := bengal
+TARGET_NO_BOOTLOADER := true
+TARGET_USES_UEFI := true
+
+# Platform
+TARGET_BOARD_PLATFORM := bengal
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno610
+QCOM_BOARD_PLATFORMS += bengal
