f91f3c70674ae1eea8bf00063939d44b97fabc5d
DY3413 <milosix3413@gmail.com>
2023-10-08 15:48:08 +0900

    Bring up TWRP recovery
    Signed-off-by: DY3413 <milosix3413@gmail.com>

--- device.mk
+++ device.mk
@@ -4,3 +4,17 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
+
+# TWRP specific build flags
+TW_THEME := portrait_hdpi
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_USE_TOOLBOX := true
+TW_INCLUDE_NTFS_3G := true
