486cc3dd8be6c5d8fb9a105f1cab6584c5ac89c4
DY3413 <milosix3413@gmail.com>
2023-10-08 13:29:07 +0900

    Inherit common configurations
    Signed-off-by: DY3413 <milosix3413@gmail.com>

--- twrp_gta4lwifi.mk
+++ twrp_gta4lwifi.mk
@@ -5,6 +5,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gta4lwifi
 PRODUCT_NAME := twrp_gta4lwifi
