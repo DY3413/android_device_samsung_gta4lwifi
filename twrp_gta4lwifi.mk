d183ce05dc38c8eb04a522a22e85a851fd613f07
DY3413 <milosix3413@gmail.com>
2023-10-08 13:22:03 +0900

    Add device identifier
    Signed-off-by: DY3413 <milosix3413@gmail.com>

--- twrp_gta4lwifi.mk
+++ twrp_gta4lwifi.mk
@@ -4,3 +4,10 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := gta4lwifi
+PRODUCT_NAME := twrp_gta4lwifi
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-T500
+PRODUCT_MANUFACTURER := samsung
