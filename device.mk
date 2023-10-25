33a5cd3216232a57f221a652afea156f0ae62a87
DY3413 <milosix3413@gmail.com>
2023-10-25 19:32:27 +0900

    Build fastbootd
    Signed-off-by: DY3413 <milosix3413@gmail.com>

--- device.mk
+++ device.mk
@@ -18,6 +18,11 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     android.system.keystore2
 
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
 # Qcom decryption
 PRODUCT_PACKAGES += \
     qcom_decrypt \
