73c34978466a0edb01ef9006cc10007e420c30ef
DY3413 <milosix3413@gmail.com>
2024-01-12 15:54:27 +0900

    Initialize device folder
    Signed-off-by: DY3413 <milosix3413@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,7 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/samsung/gta4lwifi
---
+++ twrp_gta4lwifi.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 The TWRP Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/gta4lwifi/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := gta4lwifi
+PRODUCT_NAME := twrp_gta4lwifi
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-T500
+PRODUCT_MANUFACTURER := samsung
