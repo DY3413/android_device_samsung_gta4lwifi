1f92bd877ab647949b38072bea93fc1b7dc77e91
DY3413 <milosix3413@gmail.com>
2024-01-29 15:14:53 +0900

    Add support decryption This is test so decryption is not fully working.
    Signed-off-by: DY3413 <milosix3413@gmail.com>

--- device.mk
+++ device.mk
@@ -5,3 +5,16 @@
 #
 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+# Dependencies
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion \
+    libandroidicu
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so
